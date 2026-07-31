d99a455bc3481aa1d7896996fcac85b1bc516505
ntorresland-dev <ntorresland.dev@gmail.com>
2026-07-31 13:26:41 -0400

    feat(Order): add 'get_total_price()' method

--- src/order.py
+++ src/order.py
@@ -5,3 +5,11 @@ class Order:
 
     def add_product(self, product):
         self.__products.append(product)
+
+    def get_total_price(self):
+        total = 0
+
+        for product in self.__products:
+            total += product.price 
+
+        return total
