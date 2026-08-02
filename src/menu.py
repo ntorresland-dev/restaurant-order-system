ad85a4358b912668fd627e64b300bd51ea4d8c97
ntorresland-dev <ntorresland.dev@gmail.com>
2026-08-01 22:31:51 -0400

    feat(menu): add product lookup by name

--- src/menu.py
+++ src/menu.py
@@ -4,3 +4,9 @@ class Menu:
 
     def get_products(self):
         return self.__products
+
+    def get_product(self, product):
+        for p in self.__products:
+            if product.lower() == p.name.lower():
+                return p
+        raise ValueError("El producto no se encuentra disponible.")
